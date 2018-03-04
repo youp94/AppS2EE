29724dba9d4352f0861b2a0de1b65963ab7671a3
dzabdallahpage <abdallaho98@hotmail.fr>
2018-03-04 23:12:49 +0100

    changement

---
+++ app/src/main/java/com/s2ee/etic/apps2ee/Entreprise.java
@@ -0,0 +1,44 @@
+package com.s2ee.etic.apps2ee;
+
+import java.io.Serializable;
+
+/**
+ * Created by Abdallah on 10/02/2018.
+ * Completez les attributs
+ * */
+
+public class Entreprise implements Serializable{
+    private String entreprisepic;
+    private String entreprisenom,entreprisedescrition;
+
+    public Entreprise(String entreprisepic, String entreprisenom, String entreprisedescrition) {
+        this.entreprisepic = entreprisepic;
+        this.entreprisenom = entreprisenom;
+        this.entreprisedescrition = entreprisedescrition;
+    }
+
+    public String getEntreprisepic() {
+        return entreprisepic;
+    }
+
+    public void setEntreprisepic(String entreprisepic) {
+        this.entreprisepic = entreprisepic;
+    }
+
+    public String getEntreprisenom() {
+        return entreprisenom;
+    }
+
+    public void setEntreprisenom(String entreprisenom) {
+        this.entreprisenom = entreprisenom;
+    }
+
+    public String getEntreprisedescrition() {
+        return entreprisedescrition;
+    }
+
+    public void setEntreprisedescrition(String entreprisedescrition) {
+        this.entreprisedescrition = entreprisedescrition;
+    }
+
+}
