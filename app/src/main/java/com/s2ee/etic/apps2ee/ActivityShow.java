bde14669d80801075fc86d2c72fde548ac876fac
youpes <dm_benchadi@esi.dz>
2018-05-07 01:42:57 +0100

    like button added

--- app/src/main/java/com/s2ee/etic/apps2ee/ActivityShow.java
+++ app/src/main/java/com/s2ee/etic/apps2ee/ActivityShow.java
@@ -2,6 +2,8 @@ package com.s2ee.etic.apps2ee;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -11,6 +13,7 @@ public class ActivityShow extends AppCompatActivity {
     private Entreprise Ecurrent;
     private TextView Nom,Adresse,mail,tel,web,adr,nbposte,nbstage,stagepropose,profilvise,profilrech,postprps;
     private de.hdodenhof.circleimageview.CircleImageView Pic;
+    private ImageView btnLike;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +33,14 @@ public class ActivityShow extends AppCompatActivity {
         profilvise = (TextView) findViewById(R.id.profile_vise);profilvise.setText(Ecurrent.getProfilvise());
         postprps = (TextView) findViewById(R.id.poste_propose); postprps.setText(Ecurrent.getPostPrps());
        if (Ecurrent.getEntreprisepic() != "/"){ Picasso.with(getApplicationContext()).load(Ecurrent.getEntreprisepic()).into(Pic);}
+       btnLike = findViewById(R.id.btn_dis);
+       btnLike.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Integer integer = (Integer) btnLike.getTag();
+               integer = integer == null ? 0 : integer;
+                    btnLike.setImageResource(R.drawable.like);
+           }
+       });
     }
 }
