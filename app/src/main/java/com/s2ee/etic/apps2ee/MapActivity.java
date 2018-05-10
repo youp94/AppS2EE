79c2aded91c1f7d362015cdf23f020fe4bb53e82
youpes <dm_benchadi@esi.dz>
2018-05-10 12:41:36 +0100

    problemes reglés 4

--- app/src/main/java/com/s2ee/etic/apps2ee/MapActivity.java
+++ app/src/main/java/com/s2ee/etic/apps2ee/MapActivity.java
@@ -4,8 +4,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class MapActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    ImageView img;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,26 @@ public class MapActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Carte S2EE");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        img = (ImageView) findViewById(R.id.map_carte);
+        text = (TextView) findViewById(R.id.charg);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Carte");
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String url = dataSnapshot.getValue(String.class);
+                Picasso.with(getApplicationContext()).load(url).into(img);
+                text.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     @Override
